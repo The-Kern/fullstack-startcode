af7ec229b242f76bf25debc6c40a8a34e2fdcf7a
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2021-03-11 15:36:29 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,64 @@
 import express from "express";
 //import dotenv from "dotenv";
-import path from "path"
+import path from "path";
 //dotenv.config()
 import dotenv from "dotenv";
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
+app.use(express.json());
 // Something has to go in here
 
-app.use(express.static(path.join(process.cwd(), "public")))
- 
+import FriendsFacade from "./facades/DummyDB-Facade";
+
+app.use(express.static(path.join(process.cwd(), "public")));
+
 app.get("/demo", (req, res) => {
   res.send("Server is up");
-})
+});
+
+app.post("/add", async (request, response) => {
+  try {
+    const facade = new FriendsFacade();
+    const promise = await facade.addFriend(request.body);
+
+    response.json(promise);
+  } catch (error) {
+    response.json({ message: error.message });
+  }
+});
+
+app.delete("/delete", async (request, response) => {
+  try {
+    const facade = new FriendsFacade();
+    const promise = await facade.deleteFriend(request.body.email);
+
+    console.log(facade.friends);
+    response.json(promise);
+  } catch (error) {
+    response.json({ message: error.message });
+  }
+});
+
+app.get("/all", async (request, response) => {
+  try {
+    const facade = new FriendsFacade();
+    const promise = await facade.getAllFriends();
+
+    response.json(promise);
+  } catch (error) {
+    response.json({ message: error.message });
+  }
+});
+
+app.get("/get", async (request, response) => {
+  try {
+    const facade = new FriendsFacade();
+    const promise = await facade.getFriend(request.body.email);
+
+    response.json(promise);
+  } catch (error) {
+    response.json({ message: error.message });
+  }
+});
 
 export default app;
--- src/bin/www.ts
+++ src/bin/www.ts
@@ -4,4 +4,5 @@ import app from "../app";
 const debug = require("debug")("www");
 
 const PORT = process.env.PORT || 3333;
+console.log(PORT)
 app.listen(PORT, () => debug(`Server started, listening on PORT: ${PORT}`))
