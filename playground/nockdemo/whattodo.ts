3eb27a9506c25cb15ffce714b341b243aef4d053
Sebastian Godsk Hansen <cph-sh497@cphbusiness.dk>
2021-04-11 21:27:38 +0200

    rød gul opgave

--- playground/nockdemo/whattodo.ts
+++ playground/nockdemo/whattodo.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 const app = express();
 import fetch from "node-fetch";
 
@@ -7,6 +7,30 @@ app.get("/whattodo", async (req, res) => {
     "https://www.boredapi.com/api/activity"
   ).then((r) => r.json());
   res.json(whatToDo);
+});
+
+app.get("/nameinfo/:name", async (req, res) => {
+  const { name: paramName } = req.params;
+
+  const result = await Promise.all([
+    fetch(`https://api.genderize.io?name=${paramName}`).then((res) =>
+      res.json()
+    ),
+    fetch(`https://api.nationalize.io?name=${paramName}`).then((res) =>
+      res.json()
+    ),
+    fetch(`https://api.agify.io?name=${paramName}`).then((res) => res.json()),
+  ]);
+
+  const [
+    { name, gender },
+    {
+      country: [{ country_id }],
+    },
+    { age },
+  ] = result;
+
+  res.json({ name, gender, country_id, age });
 });
 
 export default app;
